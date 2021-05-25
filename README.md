ab92b2599df1aa47bc80a6922d783be0fe4cd486
aakriti1435 <write10aakriti07@gmail.com>
2021-05-25 14:16:27 +0530

    Leads App Models

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 -   pip install django==3.1.4
 -   pip freeze > requirements.txt
 -   django-admin startproject djangoCRM
--   django-admin runserver
--   django-admin startapp leads
--   django-admin migrate
+-   python manage.py runserver
+-   python manage.py startapp leads
+-   python manage.py migrate
+-   python manage.py createsuperuser
--- leads/models.py
+++ leads/models.py
@@ -12,5 +12,12 @@ class Lead(models.Model):
     age = models.IntegerField(default=0, null=True, blank=True)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"    
+
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
