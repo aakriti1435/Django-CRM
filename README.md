33efd551ee367af2b069ba610e9dc9e8ad69220a
aakriti1435 <write10aakriti07@gmail.com>
2021-05-25 12:36:57 +0530

    Leads App Models

---
+++ README.md
@@ -0,0 +1,13 @@
+#### Commands
+
+-   pip install virtualenv
+-   virtualenv crm_env
+-   cd crm_env
+-   cd scripts
+-   activate.bat
+-   pip install django==3.1.4
+-   pip freeze > requirements.txt
+-   django-admin startproject djangoCRM
+-   django-admin runserver
+-   django-admin startapp leads
+-   django-admin migrate
--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    firstName = models.CharField(max_length=20, default="", null=True, blank=True)
+    lastName = models.CharField(max_length=20, default="", null=True, blank=True)
+    age = models.IntegerField(default=0, null=True, blank=True)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
