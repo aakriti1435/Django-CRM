ce6c897963fc22c2a79d521856e2cfd1d5a723e5
aakriti1435 <write10aakriti07@gmail.com>
2021-05-30 13:31:05 +0530

    UpdateLead

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import leadList,leadDetail,createLead
+from .views import leadList,leadDetail,createLead,updateLead
 
 app_name="leads"
 
 urlpatterns = [
     path("", leadList),
     path("create/", createLead),
-    path("<int:pk>/", leadDetail)
+    path("<int:pk>/", leadDetail),
+    path("<int:pk>/update/", updateLead)
 ]
--- leads/views.py
+++ leads/views.py
@@ -29,22 +29,41 @@ def createLead(request):
         print("POST Request")
         form = LeadModelForm(request.POST)
         if form.is_valid():
-            print("Form is Valid")
-            print(form.cleaned_data)
-            firstName = form.cleaned_data['firstName']
-            lastName = form.cleaned_data['lastName']
-            age = form.cleaned_data['age']
-            agent = form.cleaned_data['agent']
-            Lead.objects.create(
-                firstName = firstName,
-                lastName = lastName,
-                age = age,
-                agent = agent,
-            )
-            print("Lead created")
+            # firstName = form.cleaned_data['firstName']
+            # lastName = form.cleaned_data['lastName']
+            # age = form.cleaned_data['age']
+            # agent = form.cleaned_data['agent']
+            # Lead.objects.create(
+            #     firstName = firstName,
+            #     lastName = lastName,
+            #     age = age,
+            #     agent = agent,
+            # )
+            form.save()
             return redirect("/leads")
 
     context = {
         "form": form
     }
     return render(request, "leads/createLead.html", context)
+
+
+def updateLead(request, pk):
+    lead = Lead.objects.get(id=pk)
+    # form = LeadForm()
+    form = LeadModelForm(instance=lead)
+    if(request.method == "POST"):
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            # firstName = form.cleaned_data['firstName']
+            # lastName = form.cleaned_data['lastName']
+            # age = form.cleaned_data['age']
+            # lead.firstName = firstName
+            # lead.lastName = lastName
+            # lead.age = age
+            # lead.save()
+            form.save()
+            return redirect("/leads")
+
+    context={ "lead":lead, "form": form }
+    return render(request, "leads/updateLead.html", context)
