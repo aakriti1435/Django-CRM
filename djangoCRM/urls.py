f2c2824a2c1ddcf25603e99c8cf2aec059a96720
aakriti1435 <write10aakriti07@gmail.com>
2021-05-30 17:31:03 +0530

    staticFiles

--- djangoCRM/urls.py
+++ djangoCRM/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import homePage,HomePageView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', homePage, name='homePage'),
     path('', HomePageView.as_view(), name='homePage'),
     path("leads/",include('leads.urls', namespace="leads")),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
