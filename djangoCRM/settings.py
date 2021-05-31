3098234404859c70695cf59aeac0f58d3dbf7bd7
aakriti1435 <write10aakriti07@gmail.com>
2021-05-31 22:25:01 +0530

    Crispy Forms

--- djangoCRM/settings.py
+++ djangoCRM/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_tailwind',
     'leads',
     'agents'
 ]
@@ -134,3 +136,6 @@ LOGIN_REDIRECT_URL = "/leads"
 LOGIN_URL = "/login"
 
 LOGOUT_REDIRECT_URL = "/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
+CRISPY_TEMPLATE_PACK = 'tailwind'
